91b1d14d0e4b191119ad6b3d9011aaa9819fae8c
Yuliia_Sheremeta <sheremeyulya1996@gmail.com>
2025-01-19 23:39:55 +0100

    add changes

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,12 @@
-fetch('https://pixabay.com/api/?q=${searchedQuery}&key=48292364-ad13d53928d4b39a49844bb07&image_type=photo&orientation=horizontal&safesearch=true').then(response => {
-    if (!response.ok)
-    { throw new Error(response.status); }
-     return response.json();
-}).then(data => {
-    if (data.results.length === 0) {
-        alert('"Sorry, there are no images matching your search query. Please try again!"');
-        galleryEl.innerHTML = '';
-        formSearch.reset();
-        return;
-     };
-    const galleryTemplate = data.results.map(el => createGalleryCardTemplate(el)).join('');
-    galleryEl.innerHTML = galleryTemplate;
- }).catch(err => {
-    console.log(err);});
+const myApiKey = '48292364-ad13d53928d4b39a49844bb07';
+const urlApiBase = 'https://pixabay.com/api/';
+
+export const fetchImages = async (searchedQuery) => {
+    const response = await fetch(`${urlApiBase}?q=${searchedQuery}&key=${myApiKey}&image_type=photo&orientation=horizontal&safesearch=true`);
+    
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    
+    return response.json();
+};
--- src/main.js
+++ src/main.js
@@ -1,46 +1,47 @@
-import iziToast from "izitoast";
 import SimpleLightbox from "simplelightbox";
+import { fetchImages } from './js/pixabay-api.js';
+import { createGalleryCardTemplate, showAlert } from './js/render-functions.js';
 
 const formSearch = document.querySelector('.form');
 const galleryEl = document.querySelector('.search-list');
-
-const createGalleryCardTemplate = imgInfo => {
-    return `<li class="gallery-card"><a href = "${imgInfo.largeImageURL}" > <img src="${imgInfo.webformatURL}" alt="${imgInfo.tags}"/><a/> <div class="info"><p>Likes: ${imgInfo.likes}</p><p>Views: ${imgInfo.views}</p><p>Comments: ${imgInfo.comments}</p><p>Downloads: ${imgInfo.downloads}</p></div > </li > `
-};
+const loader = document.querySelector('.loader');
 
 const onFormSubmit = event => {
     event.preventDefault();
     const searchedQuery = event.currentTarget.elements.search.value.trim();
-    
+
     if (searchedQuery === '') {
-        alert('Enter text to search!');
+        showAlert('Enter text to search!');
         return;
-    };
+    }
 
-   fetch(`https://pixabay.com/api/?q=${searchedQuery}&key=48292364-ad13d53928d4b39a49844bb07&image_type=photo&orientation=horizontal&safesearch=true`).then(response => {
-    if (!response.ok)
-    { throw new Error(response.status); }
-       return response.json();
-       
-}).then(data => {
-    if (data.hits.length === 0) {
-        alert('"Sorry, there are no images matching your search query. Please try again!"');
-        galleryEl.innerHTML = '';
-        formSearch.reset();
-        return;
-    };
-    
-    const galleryTemplate = data.hits.map(el => createGalleryCardTemplate(el)).join('');
+    loader.classList.add('active');
 
-    galleryEl.innerHTML = galleryTemplate;
-    
-    const lightbox = new SimpleLightbox('.gallery-card a', {
+    fetchImages(searchedQuery).then(data => {
+        if (data.hits.length === 0) {
+            showAlert('Sorry, there are no images matching your search query. Please try again!');
+            galleryEl.innerHTML = '';
+            formSearch.reset();
+            return;
+        }
+
+        const galleryTemplate = data.hits.map(el => createGalleryCardTemplate(el)).join('');
+        galleryEl.innerHTML = galleryTemplate;
+
+        const lightbox = new SimpleLightbox('.gallery-card a', {
             captionDelay: 250,
             captionsData: 'alt',
         });
- }).catch(err => {
-    console.error(err);});
 
-
+        lightbox.refresh();
+    }).catch(err => {
+        console.error(err);
+    }).finally(() => {
+        setTimeout(() => {
+            loader.classList.remove('active');
+        }, 1000);
+    });
+        
 };
+
 formSearch.addEventListener('submit', onFormSubmit);
